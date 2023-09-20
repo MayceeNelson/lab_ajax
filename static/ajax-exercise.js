669114f043f72a4698c41d25ccd2374ba664ae66
Maycee Nelson <Mayceenelson@yahoo.com>
2023-09-20 17:00:33 -0600

    finished lab

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -35,15 +35,40 @@ function showWeather(evt) {
   // TODO: request weather with that URL and show the forecast in #weather-info
 }
 
-
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
 // PART 3: ORDER MELONS
-
 function orderMelons(evt) {
+  
   evt.preventDefault();
+  
+  const formInputs = {
+    melon_type: document.querySelector('#melon-type-field').value,
+    qty: document.querySelector('#qty-field').value,
+  }
 
-  // TODO: show the result message after your form
-  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+  fetch('/order-melons.json', {
+    method: 'POST',
+    body: JSON.stringify(formInputs),
+    headers: {
+    'Content-Type': 'application/json',
+    },
+  })
+  .then((response) => response.json())
+  .then((orderResponse) => {
+    // console.log(responseJson);
+    if (orderResponse['code'] === 'ERROR'){
+        const error = document.querySelector('#order-status');
+        
+        error.setAttribute('class', '.order-error');
+        error.innerHTML = orderResponse['msg']; // red
+    }
+    else {
+      document.querySelector('#order-status').innerHTML = orderResponse['msg'];
+
+    }
+    // normal
+      // put responseJson into HTML
+  });
 }
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
